5c74c4b5c05bd42a08eede93afcc5cf346c00ccb
IgorYashch <yashch.igor@gmail.com>
2023-01-03 02:03:31 +0300

    Add ReLU actiovation module

--- MyTorch/nn/modules/activation.py
+++ MyTorch/nn/modules/activation.py
@@ -1,7 +1,15 @@
 from .module import Module
 
 
-__all__ = ['Tanh', 'Sigmoid']
+__all__ = ['Tanh', 'Sigmoid', 'ReLU']
+
+
+
+class ReLU(Module):
+    def __init__(self):
+        super().__init__()
+    def forward(self, input):
+        return input.relu()
 
 
 class Tanh(Module):
